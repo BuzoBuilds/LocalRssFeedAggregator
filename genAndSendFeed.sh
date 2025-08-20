da9801f428d1f820335ba9d7ad540caded6adc80
BuzoBuilds <buzobuilds@gmail.com>
2025-08-20 09:06:25 -0400

    quite mvn output for auto job

--- genAndSendFeed.sh
+++ genAndSendFeed.sh
@@ -2,6 +2,6 @@
 echo "*** RSS AGGREGATOR JOB RUN; TIME: $(date) ***"
 PROJROOTDIR=$(dirname $0); 
 cd $PROJROOTDIR/RssFeedAggregatorApp 
-echo -e "sendall\nquit\n" | mvn exec:java -Dexec.mainClass="com.buzoTechie.RssFeedAggregator.App"
+echo -e "sendall\nquit\n" | mvn -q exec:java -Dexec.mainClass="com.buzoTechie.RssFeedAggregator.App"
 echo "*** RSS AGGREGATOR JOB COMPLETED ***"
 
